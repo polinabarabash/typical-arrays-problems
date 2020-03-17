762c8b347ef62ab53881901571dd951de69e0d33
Polina Barabash <polina_11.92@inbox.ru>
2020-03-17 22:31:37 +0300

    feat: implement typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) {  
+  if (array === undefined  || array.length === 0) {
+    return 0;
+  }
+  else {
+    let min = 0;
+    if (array[0] >= array[1]) {
+      min = array[1];
+    } else min = array[0];
+    array.forEach(element => {
+      if (element <= min) {
+        min = element;
+      } 
+    });return min;
+  }  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined  || array.length === 0) {
+    return 0;
+  } 
+  else {
+    let max = 0;
+    array.forEach(element => {
+      if (element > max) {
+        max = element;
+      }
+    }); return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined  || array.length === 0) {
+    return 0;
+  }
+  else {
+    let result = 0;
+    array.forEach(element => {
+    result += element;
+    });
+    return result / array.length;
+  }
+  
 }
